56f2e972fb8079ef7c2136d68cf628192420c721
RobertBauer01 <bauer.robert@iisgalvanimi.edu.it>
2023-12-31 22:14:59 +0000

    Seconda parte

---
+++ routing-test-app2/src/app/emoji.model.ts
@@ -0,0 +1,19 @@
+export interface Emoji {
+name : string;
+emoji : string;
+};
+export const FRUITS = [
+    { name: 'Apple', emoji: '🍎' },
+    { name: 'Banana', emoji: '🍌' },
+    { name: 'Orange', emoji: '🍊' },
+    { name: 'Grapes', emoji: '🍇' },
+    
+  ];
+
+export const ANIMALS = [
+    { name: '(ﾉಥ益ಥ)ﾉ', emoji: '🐒' },
+    { name: 'kelb', emoji: '🐶' },
+    { name: 'dib', emoji: '🐺' },
+    { name: 'mesh', emoji: '🐱' },
+    
+];
